8f928e520a68943c6136afa9707ab4fe5641bec7
Diego <dlandrade2103@gmail.com>
2019-12-16 18:40:26 +0000

    first commit

---
+++ app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('accounts/', include('allauth.urls')),
+    path('', views.home, name='home'),
+]
+
+urlpatterns += staticfiles_urlpatterns()
